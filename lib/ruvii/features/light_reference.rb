92e423e36e60861d1572d610914c66efe879e267
Ian MacLeod <ian@nevir.net>
2012-04-09 09:04:35 -0700

    const_ref()

---
+++ lib/ruvii/features/light_reference.rb
@@ -0,0 +1,51 @@
+# # Class Reference
+require "ruvii/dependencies"
+
+class Ruvii::LightReference < BasicObject
+
+  def initialize(const)
+    if const.respond_to?(:name) && const.name
+      @split_name = const.name.to_s.split("::").map(&:to_sym)
+    end
+
+    unless ::Ruvii::LightReference.can_resolve? @split_name
+      ::Kernel.raise ::TypeError, "Ruvii::LightReference can only manage named constants!"
+    end
+  end
+
+  def method_missing(sym, *args, &block)
+    ::Ruvii::LightReference.resolve(@split_name).send(sym, *args, &block)
+  end
+
+  class << self
+
+    def can_resolve?(split_name)
+      return false unless name
+
+      steps = [::Object]
+      Array(split_name).each { |n| steps << (steps.last ? steps.last.const_get(n) : nil) }
+
+      steps.size > 1 && steps.all?
+    end
+
+    def resolve(split_name)
+      base = ::Object
+      split_name.each do |name|
+        break unless base
+        base = base.const_get(name)
+      end
+
+      base
+    end
+
+  end
+
+end
+
+module Ruvii::LightReferenceHelper
+  def const_ref(*args)
+    ::Ruvii::LightReference.new(*args)
+  end
+end
+
+Object.send :include, Ruvii::LightReferenceHelper
---
+++ spec/features/light_reference_spec.rb
@@ -0,0 +1,88 @@
+require "ruvii/features/light_reference"
+
+module A
+  module B
+    module C
+      class D
+        module E
+          def self.thing
+            @thing ||= 0
+            @thing  += 1
+          end
+        end
+      end
+    end
+  end
+end
+
+describe Ruvii::LightReference do
+
+  it "should provide a convenient and nearly transparent wrapper to a class" do
+    str_ref = described_class.new(String)
+
+    str_ref.inspect.should == "String"
+    str_ref.new.should == ""
+    str_ref.object_id.should == String.object_id
+  end
+
+  it "should not freak out if you ask it to wrap Object" do
+    obj_ref = described_class.new(Object)
+    obj_ref.inspect.should == "Object"
+  end
+
+  it "should raise when you pass a non-constant" do
+    expect { described_class.new("") }.to raise_error(TypeError)
+  end
+
+  it "should raise if you give it an unreferencable constant" do
+    expect { described_class.new(Class.new) }.to raise_error(TypeError)
+  end
+
+  it "should find deeply nested constants" do
+    deep_ref = described_class.new(A::B::C::D::E)
+    deep_ref.name.should == "A::B::C::D::E"
+  end
+
+  it "should provide a helper method for sane use" do
+    const_ref(A::B::C::D::E).name.should == "A::B::C::D::E"
+  end
+
+  it "should work when you yank the constant out from under it" do
+    module Foo; end
+    orig_id = Foo.object_id
+
+    foo_ref = described_class.new(Foo)
+    foo_ref.object_id.should == orig_id
+
+    Object.send :remove_const, :Foo
+    module Foo; end
+
+    Foo.object_id.should_not == orig_id
+    foo_ref.object_id.should == Foo.object_id
+  end
+
+  it "should raise an appropriate error if we yank the constant out and don't replace it" do
+    module Foo; end
+    foo_ref = described_class.new(Foo)
+
+    Object.send :remove_const, :Foo
+
+    expect { foo_ref.inspect }.to raise_error(NameError, "uninitialized constant Foo")
+  end
+
+  it "should be fast" do
+    deep_ref = described_class.new(A::B::C::D::E)
+
+    start = Time.now
+    10000.times { A::B::C::D::E.thing }
+    naked_duration = Time.now - start
+
+    start = Time.now
+    10000.times { deep_ref.thing }
+    wrapped_duration = Time.now - start
+
+    call_overhead = wrapped_duration / naked_duration
+    call_overhead.should <= 7.5
+  end
+
+end
